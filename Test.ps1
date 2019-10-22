b9f7e028c94836352acb6e2a588faa14343b0c92
closeha <56692941+closeha@users.noreply.github.com>
2019-10-22 13:24:15 -0700

    Second Update

--- Test.ps1
+++ Test.ps1
@@ -1,22 +1,2 @@
 adfsdf
-function Get-TargetResource {
-    # TODO: Add parameters here
-    # Make sure to use the same parameters for
-    # Get-TargetResource, Set-TargetResource, and Test-TargetResource
-    param (
-    )
-}
-function Set-TargetResource {
-    # TODO: Add parameters here
-    # Make sure to use the same parameters for
-    # Get-TargetResource, Set-TargetResource, and Test-TargetResource
-    param (
-    )
-}
-function Test-TargetResource {
-    # TODO: Add parameters here
-    # Make sure to use the same parameters for
-    # Get-TargetResource, Set-TargetResource, and Test-TargetResource
-    param (
-    )
-}adsadsf
+adsadsf
